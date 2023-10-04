b9fb2a3447331c263726365e7aa00af2be1413c5
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-03 22:05:01 -0500

    Create view-playstyle.php

---
+++ view-playstyle.php
@@ -0,0 +1,27 @@
+<h1>Champions</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Lane</th>
+      <th>Powerspike</th>
+      <th>Damage</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($playstyles = $playstyle->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $playstyles['playstyle_id']; ?></td>
+    <td><?php echo $playstyles['lane']; ?></td>
+    <td><?php echo $playstyles['powerspike']; ?></td>
+    <td><?php echo $playstyles['damage']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
