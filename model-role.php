993eb349e231e585f6445f1d8374a70b34b99fda
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-03 21:53:22 -0500

    Create model-role.php

---
+++ model-role.php
@@ -0,0 +1,15 @@
+<?php
+function selectRole() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT role_id, role_type, role_difficulty FROM `champion`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
