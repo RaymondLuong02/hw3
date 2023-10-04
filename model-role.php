b5527406e83f1beb151c3187ed2aac1012a17e0e
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-03 22:56:06 -0500

    Update model-role.php

--- model-role.php
+++ model-role.php
@@ -2,7 +2,7 @@
 function selectRole() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT role_id, role_type, role_difficutly FROM `role`");
+        $stmt = $conn->prepare("SELECT role_id, role_type, role_difficultly FROM `role`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
