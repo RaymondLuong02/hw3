6d40749abc2b196b1eccf82fda2ef486a1ccce61
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-03 21:54:53 -0500

    Create view-role.php

---
+++ view-role.php
@@ -0,0 +1,25 @@
+<h1>Roles</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Type</th>
+      <th>Difficulty</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($roles = $role->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $roles['role_id']; ?></td>
+    <td><?php echo $roles['role_type']; ?></td>
+    <td><?php echo $roles['role_difficulty']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
