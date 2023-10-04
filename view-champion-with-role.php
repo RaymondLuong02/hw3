516adb3c8b85ea6e8bcb21f7f007569efe8fbc49
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-04 00:41:32 -0500

    Update view-champion-with-role.php

--- view-champion-with-role.php
+++ view-champion-with-role.php
@@ -8,13 +8,16 @@ while ($champions = $champion->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $champions['champion_name']; ?></h5>
       <p class="card-text">
+        <ul class="list-group">
 <?php
   $roles = selectRoleByChampion($champions['champion_id']);
   while ($role = $roles->fetch_assoc()){
 ?>
+      <li class="list-group-item"><?php echo $roles['role_type']; ?> - <?php echo $roles['lane']; ?> - <?php echo $roles['damage']; ?> - <?php echo $roles['powerspike']; ?></li>
 <?php
   }
 ?>
+    </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Color: <?php echo $champions['champion_color']; ?></small></p>
     </div>
