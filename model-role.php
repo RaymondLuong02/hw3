dfe1ff7e21c33c6c26d7a50b76d75de6f4f2d9dd
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-24 19:46:49 -0500

    Update model-role.php

--- model-role.php
+++ model-role.php
@@ -12,4 +12,49 @@ function selectRole() {
         throw $e;
     }
 }
+
+function insertRole($cRole, $cDiff) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `role` (`role_type`, `role_difficulty`) VALUES (?, ?");
+        $stmt->bind_param("ss", $cRole, $cDiff);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateRole($cRole, $cDiff, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `role` set `role_type` = ?, `role_difficulty`= ? where role_id = ?");
+        $stmt->bind_param("ssi", $cRole, $cDiff, $cid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteRole($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from role where role_id=?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
