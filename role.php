3e5a7c1f1db54df65eabf55992d12cba37503927
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-24 23:06:05 -0500

    Update role.php

--- role.php
+++ role.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
          echo '<div class="alert alert-danger" role="alert">Error. </div>';
       }
       break;
+        case "Edit":
+      if (updateRole($_POST['cRole'], $_POST['cDiff'], $_POST['cid])){
+        echo '<div class="alert alert-success" role="alert">Role Edited. </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert">Error. </div>';
+      }
+      break;
         case "Delete":
       if (deleteRole($_POST['cid'])){
         echo '<div class="alert alert-success" role="alert">Role Deleted. </div>';
