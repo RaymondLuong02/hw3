2f0fdae7f6caebe75162384672ddc94c085e26f2
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-24 22:23:18 -0500

    Update role.php

--- role.php
+++ role.php
@@ -4,6 +4,14 @@ require_once("model-role.php");
 
 $pageTitle = "Role";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($POST['actionType']) {
+    case "Add":
+      insertRole($_POST['cRole'], $_POST['cDiff']);
+      break;
+    }
+}
 $role = selectRole();
 include "view-role.php";
 include "view-footer.php";
