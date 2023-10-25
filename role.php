7f18db5cbfe6cd276faa0ed9b2f39c7989821919
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-24 22:42:42 -0500

    Update role.php

--- role.php
+++ role.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
          echo '<div class="alert alert-danger" role="alert">Error. </div>';
       }
       break;
+        case "Delete":
+      if (deleteRole($_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Role Deleted. </div>';
+      } else {
+         echo '<div class="alert alert-danger" role="alert">Error. </div>';
+      }
+      break;
     }
 }
 $role = selectRole();
