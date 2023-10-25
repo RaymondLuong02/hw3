74804a39e31022ff6591085c8a32abe0f686b069
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-24 23:48:03 -0500

    Update model-champion-with-role.php

--- model-champion-with-role.php
+++ model-champion-with-role.php
@@ -27,4 +27,74 @@ function selectRoleByChampion($iid) {
         throw $e;
     }
 }
+
+function selectChampionForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT champion_id, champion_name FROM 'champion' order by champion_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectRoleForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT role_id, role_type, FROM 'role' order by role_type");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertPlayStyle($iid, $cid, $sem, $room, $time) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("");
+        $stmt->bind_param("iisss", $iid, $cid, $sem, $room, $time);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatePlayStyle($iid, $cid, $sem, $room, $time) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("");
+        $stmt->bind_param("iisssi", $iid, $cid, $sem, $room, $time, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePlayStyle($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from section where playstyle_id=?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
