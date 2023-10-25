df83c3763b0327d4efeefb7d5fc1e352333409a5
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-10-24 22:41:32 -0500

    Update view-role.php

--- view-role.php
+++ view-role.php
@@ -16,6 +16,8 @@ include "view-role-newform.php";
       <th>Type</th>
       <th>Difficulty</th>
         <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -26,6 +28,19 @@ while ($roles = $role->fetch_assoc()) {
     <td><?php echo $roles['role_id']; ?></td>
     <td><?php echo $roles['role_type']; ?></td>
     <td><?php echo $roles['role_difficulty']; ?></td>
+    <td>
+    </td>
+    <td>
+            <form method="post" action="">
+        <input type = "hidden" name="cid" value="<?php echo $roles['role_id']; ?>">
+              <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+        </svg>
+        </button>
+      </form>
+    </td>
     <td>
       <form method="post" action="playstyle-by-role.php">
         <input type = "hidden" name="cid" value="<?php echo $roles['role_id']; ?>">
