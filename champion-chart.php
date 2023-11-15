3d2698ce9691179a64300b279422180d022d7a0b
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-14 18:36:46 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -32,4 +32,17 @@ include "view-footer.php";
     ]
 },
   });
+
+    new Chart(ctx, {
+    type: 'bar',
+    data:  {
+    datasets: [{
+        barPercentage: 0.5,
+        barThickness: 6,
+        maxBarThickness: 8,
+        minBarLength: 2,
+        data: [10, 20, 30, 40, 50, 60, 70]
+    }]
+},
+  });
 </script>
