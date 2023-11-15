f4b219f8eba699ade8a5e4800d36b426b3cf1d2d
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-14 19:05:33 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -67,3 +67,67 @@ include "view-footer.php";
     }
   });
 </script>
+
+
+<div>
+  <canvas id="myChart3"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<?php
+include "view-footer.php";
+?>
+<script>
+  const ctx3 = document.getElementById('myChart3');
+
+  new Chart(ctx3, {
+    type: 'bubble',
+    data: {
+      datasets: [{
+        label: 'Bubble Chart',
+        data: [
+          { x: 10, y: 20, r: 5 },
+          { x: 15, y: 25, r: 8 },
+          { x: 20, y: 30, r: 12 },
+          // Add more data points as needed
+        ],
+        backgroundColor: 'rgba(75, 192, 192, 0.6)', // Set bubble color
+      }]
+    },
+  });
+</script>
+
+
+<div>
+  <canvas id="myChart4"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<?php
+include "view-footer.php";
+?>
+<script>
+  const ctx4 = document.getElementById('myChart4');
+
+  new Chart(ctx4, {
+    type: 'bar',
+    data: {
+      labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5', 'Label 6', 'Label 7'],
+      datasets: [{
+        data: [10, 20, 30, 40, 50, 60, 70],
+        backgroundColor: 'blue',
+        barPercentage: 0.5,
+        barThickness: 6,
+        maxBarThickness: 8,
+        minBarLength: 2,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
