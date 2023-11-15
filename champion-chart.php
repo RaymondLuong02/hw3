7caa0d3cd2f30032a04e6fa2c3ba0d139fb708c3
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-14 18:52:26 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -31,4 +31,35 @@ include "view-footer.php";
     ]
 },
   });
+
+
+
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<?php
+include "view-footer.php";
+?>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx2 = document.getElementById('myChart2');
+
+  new Chart(ctx2, {
+    type: 'doughnut',
+    data:  {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Green',
+        'Red',
+        'Yellow'
+    ]
+},
+  });
 </script>
