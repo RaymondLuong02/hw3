3938cc681d593c76b01bf79e166533a3c86c63e3
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-15 14:15:34 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -36,40 +36,31 @@ include "view-footer.php";
 
 
 
-<div>
-  <canvas id="myChart2" style="width:100%;max-width:700px"></canvas>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+<div id="myPlot" style="width:100%;max-width:700px"></div>
+
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
 <?php
 include "view-footer.php";
 ?>
 <script>
-const xValues = [100,200,300,400,500,600,700,800,900,1000];
+const xArray = [50,60,70,80,90,100,110,120,130,140,150];
+const yArray = [7,8,8,9,9,9,10,11,14,14,15];
 
-new Chart("myChart2", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-      borderColor: "red",
-      fill: false
-    },{
-      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-      borderColor: "green",
-      fill: false
-    },{
-      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
-  }
-});
+const data = [{
+  x:xArray,
+  y:yArray,
+  mode:"markers"
+}];
+
+const layout = {
+  xaxis: {range: [40, 160], title: "Square Meters"},
+  yaxis: {range: [5, 16], title: "Price in Millions"},  
+  title: "House Prices vs. Size"
+};
+Plotly.newPlot("myPlot", data, layout);
 </script>
+
 
 
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
