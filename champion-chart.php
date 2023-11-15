fe0255ce9323d3247f47a2aa41f1ea9927827b63
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-14 18:37:50 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -32,17 +32,19 @@ include "view-footer.php";
     ]
 },
   });
-
-    new Chart(ctx, {
-    type: 'bar',
+  new Chart(ctx, {
+    type: 'doughnut',
     data:  {
     datasets: [{
-        barPercentage: 0.5,
-        barThickness: 6,
-        maxBarThickness: 8,
-        minBarLength: 2,
-        data: [10, 20, 30, 40, 50, 60, 70]
-    }]
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
 },
   });
 </script>
