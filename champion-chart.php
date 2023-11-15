8a6514db5115cd44d925e8adf1aa8ba167c1276f
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-14 18:31:26 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -3,7 +3,33 @@ $pageTitle = "Charts";
 include "view-header.php";
 ?>
 <h1>Charts</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <?php
 include "view-footer.php";
 ?>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data:  {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+},
+  });
+</script>
