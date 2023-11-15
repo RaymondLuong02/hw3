3d93e306bcdac53f71d9f8d253ec13e748e476f0
RaymondLuong02 <122938119+RaymondLuong02@users.noreply.github.com>
2023-11-14 19:08:13 -0600

    Update champion-chart.php

--- champion-chart.php
+++ champion-chart.php
@@ -111,17 +111,21 @@ include "view-footer.php";
 
   new Chart(ctx4, {
     type: 'polarArea',
-    data: 
-    datasets: [{
-        data: [10, 20, 30]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
+    data: {
+      datasets: [{
+        data: [10, 20, 30],
+        backgroundColor: [
+          'red',
+          'yellow',
+          'blue'
+        ]
+      }],
+      labels: [
         'Red',
         'Yellow',
         'Blue'
-    ]
-},
-  );
+      ]
+    }
+  });
 </script>
+
